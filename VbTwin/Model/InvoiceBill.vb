23c9d7f6cfd3f8e6e472e8279c86da3a7df5d064
IronHead <rouninlopez@gmail.com>
2021-02-08 17:32:16 -0600

    ammended namespace on all models to "models"

--- VbTwin/Model/InvoiceBill.vb
+++ VbTwin/Model/InvoiceBill.vb
@@ -1,6 +1,8 @@
-﻿Public Class InvoiceBill
+﻿Namespace Model
+    Public Class InvoiceBill
 #Region "Properties"
-    Public AddressLine As String = String.Empty
-    Public InvoiceLine As String = String.Empty
+        Public AddressLine As String = String.Empty
+        Public InvoiceLine As String = String.Empty
 #End Region
-End Class
+    End Class
+End Namespace
