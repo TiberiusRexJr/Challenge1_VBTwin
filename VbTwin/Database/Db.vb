4c75ec72b56817fc570b6be3e8e1584cd5a14475
Lexus <rounin_lopez@yahoo.com>
2021-02-21 23:07:24 -0600

    database code complete

--- VbTwin/Database/Db.vb
+++ VbTwin/Database/Db.vb
@@ -74,11 +74,28 @@ Public Class Db
 
                     If customerCommand.ExecuteNonQuery() = 1 Then
                         b.BillInfo.CustomerID = Convert.ToInt32(queryIdentityCommand.ExecuteScalar())
+
+                        Dim billCommand As OleDbCommand = PrepareBillCommand(b)
+                        If billCommand.ExecuteNonQuery() = 1 Then
+                            status = True
+                        Else
+                            Return status = False
+                        End If
+
+
+                    Else
+                        Return status = False
+
                     End If
+
                 Next
             Catch ex As Exception
-
+                Console.WriteLine(ex.Message)
+                Return status = False
+            Finally
+                _conn.Close()
             End Try
+
 
             Return status
         End Function
@@ -87,7 +104,50 @@ Public Class Db
         ''' </summary>
         ''' <returns></returns>
         Public Function GetData() As List(Of BillHeader)
+
             Dim returnData As List(Of BillHeader) = Nothing
+
+            Dim getDataQuery As String = "SELECT b.CustomerID,c.CustomerName,c.AccountNumber,c.CustomerAddress,c.CustomerCity,c.CustomerState,c.CustomerZip,b.ID,b.BillDate,b.BillNumber,b.AccountBalance,b.DueDate,b.BillAmount,b.FormatGUID,c.DateAdded FROM Customer AS c Inner JOIN Bills as b ON c.ID=b.CustomerID"
+
+            Dim getDataCommand = New OleDbCommand(getDataQuery, _conn)
+
+            Try
+                _conn.Open()
+
+                Dim reader As OleDbDataReader = getDataCommand.ExecuteReader()
+
+                While reader.Read()
+
+                    Dim b As BillHeader = New BillHeader()
+
+                    b.BillInfo.CustomerID = Convert.ToInt32(reader("CustomerName").ToString())
+                    b.CustomerName = reader("CustomerName").ToString()
+                    b.AccountNo = reader("AccountNumber").ToString()
+                    b.AddressInformation.Mailing_Address_1 = reader("CustomerAddress").ToString()
+                    b.AddressInformation.City = reader("CustomerCity").ToString()
+                    b.AddressInformation.State = reader("CustomerState").ToString()
+                    b.AddressInformation.Zip = reader("CustomerZip").ToString()
+
+                    b.BillInfo.ID = Convert.ToInt32(reader("ID").ToString())
+                    b.BillDate = Convert.ToDateTime(reader("BillDate"))
+                    b.BillInfo.BillNumber = reader("BillNumber").ToString()
+                    b.BillInfo.BalanceDue = Convert.ToDecimal(reader("AccountBalance"))
+                    b.DueDate = Convert.ToDateTime(reader("DueDate"))
+                    b.BillInfo.BillAmount = Convert.ToDecimal(reader("BillAmount").ToString())
+                    b.BillInfo.FormateGUID = reader("FormatGUID").ToString()
+                    b.DateAdded = Convert.ToDateTime(reader("DateAdded"))
+
+                    returnData.Add(b)
+
+                End While
+
+
+            Catch ex As Exception
+                Console.WriteLine(ex.Message.ToString)
+                Return returnData
+            Finally
+                _conn.Close()
+            End Try
 
             Return returnData
         End Function
@@ -100,6 +160,25 @@ Public Class Db
         Private Function PrepareCustomerCommand(ByVal billData As BillHeader) As OleDbCommand
 
             Dim returnCommand As OleDbCommand = Nothing
+            Dim queryInsertIntoCustomer = "Insert into Customer(CustomerName,AccountNumber,CustomerAddress,CustomerCity,CustomerState,CustomerZip,DateAdded) VALUES(?,?,?,?,?,?,?)"
+            Dim customerCommand As OleDbCommand = Nothing
+
+            Try
+                customerCommand = New OleDbCommand(queryInsertIntoCustomer, _conn)
+                billData.DateAdded = DateTime.Today
+
+                customerCommand.Parameters.AddWithValue("@CustomerName", billData.CustomerName)
+                customerCommand.Parameters.AddWithValue("@AccountNumber", billData.AccountNo)
+                customerCommand.Parameters.AddWithValue("@CustomerAddress", billData.AddressInformation.Mailing_Address_1)
+                customerCommand.Parameters.AddWithValue("@CustomerCity", billData.AddressInformation.City)
+                customerCommand.Parameters.AddWithValue("@CustomerState", billData.AddressInformation.State)
+                customerCommand.Parameters.AddWithValue("@CustomerZip", billData.AddressInformation.Zip)
+                customerCommand.Parameters.AddWithValue("@DateAdded", billData.DateAdded)
+
+            Catch ex As Exception
+                Console.WriteLine(ex.Message)
+                Return returnCommand
+            End Try
 
             Return returnCommand
         End Function
@@ -112,8 +191,22 @@ Public Class Db
         Private Function PrepareBillCommand(ByVal b As BillHeader) As OleDbCommand
 
             Dim returnCommand As OleDbCommand = Nothing
+            Dim queryInsertIntoBill As String = "Insert into Bills(BillDate,BillNumber,BillAmount,FormatGUID,AccountBalance,DueDate,ServiceAddress,FirstEmailDate,SecondEmailDate,CustomerID) VALUES(?,?,?,?,?,?,?,?,?,?)"
 
-            Throw New NotImplementedException()
+            Dim billCommand As OleDbCommand = New OleDbCommand(queryInsertIntoBill, _conn)
+
+            billCommand.Parameters.AddWithValue("@BillDate", b.BillDate)
+            billCommand.Parameters.AddWithValue("@BillNumber", b.InvoiceNo)
+            billCommand.Parameters.AddWithValue("@BillAmount", b.BillInfo.BillAmount)
+            billCommand.Parameters.AddWithValue("@FormatGUID", b.FromatGUID)
+            billCommand.Parameters.AddWithValue("@AccountBalance", b.BillInfo.BalanceDue)
+            billCommand.Parameters.AddWithValue("@DueDate", b.DueDate)
+            billCommand.Parameters.AddWithValue("@ServiceAddress", b.SERVICE_ADDRESS)
+            billCommand.Parameters.AddWithValue("@FirstEmailDate", b.BillInfo.FirstEmailDate)
+            billCommand.Parameters.AddWithValue("@SecondEmailDate", b.BillInfo.SecondEmailDate)
+            billCommand.Parameters.AddWithValue("@CustomerID", b.BillInfo.CustomerID)
+
+            Return returnCommand
         End Function
 
 
